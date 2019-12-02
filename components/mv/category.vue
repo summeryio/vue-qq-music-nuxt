e534d2a342caee42b0135651df248993ec496333
summeryio <1005526074@qq.com>
2019-12-02 18:35:38 +0800

    update

--- components/mv/category.vue
+++ components/mv/category.vue
@@ -1,22 +1,15 @@
 <template>
     <div class="category">
-        <!-- <dl>
+        <dl>
             <dt>区域</dt>
             <dd>
-                <a href="javascript: void(0);" :class="{on: areaIndex === a}" v-for="(area, a) in areas" :key="a">{{area}}</a>
+                <a href="javascript: void(0);" :class="{on: areaIndex === a}" v-for="(area, a) in areas" :key="a" @click="areaSelect(a)">{{area}}</a>
             </dd>
         </dl>
         <dl>
             <dt>版本</dt>
             <dd>
-                <a href="javascript: void(0);" :class="{on: typeIndex === t}" v-for="(type, t) in types" :key="t">{{type}}</a>
-            </dd>
-        </dl> -->
-
-        <dl v-for="(cat, c) in categories" :key="c">
-            <dt>{{cat.t}}</dt>
-            <dd>
-                <a href="javascript: void(0);" :class="{on: tagIndex === i}" v-for="(item, i) in cat.arr" :key="i">{{item}}</a>
+                <a href="javascript: void(0);" :class="{on: typeIndex === t}" v-for="(type, t) in types" :key="t" @click="typeSelect(t)">{{type}}</a>
             </dd>
         </dl>
     </div>
@@ -28,24 +21,19 @@
 export default {
     data() {
         return {
-            // areas: ['全部', '内地', '港台', '欧美', '日本', '韩国'],
-            // areaIndex: 0,
-            // types: ['全部', '官方版', '原生', '现场版', '网易出品'],
-            // typeIndex: 0,
-
-            categories: [
-                {
-                    t: '区域',
-                    arr: ['全部', '内地', '港台', '欧美', '日本', '韩国']
-                },
-                {
-                    t: '版本',
-                    arr: ['全部', '官方版', '原生', '现场版', '网易出品']
-                }
-            ],
-            catIndex: 0,
-            tagIndex: 0
+            areas: ['全部', '内地', '港台', '欧美', '日本', '韩国'],
+            areaIndex: 0,
+            types: ['全部', '官方版', '原生', '现场版', '网易出品'],
+            typeIndex: 0,
         }
+    },
+    methods: {
+        areaSelect(index) {
+            this.areaIndex = index
+        },
+        typeSelect(index) {
+            this.typeIndex = index
+        },
     }
 }
 </script>
--- store/state.js
+++ store/state.js
@@ -2,6 +2,12 @@
 const state = {
   playlistSelectedTag: null,
   playlistOrder: 'hot',
+
+  mv: {
+    areaTag: null,
+    typeTag: null,
+    order: null
+  }
 }
 
 export default state
